0ea46919cb2ce5fe6ac3375b40eba37c3402c6ef
Naji2402 <najisinan2402@gmail.com>
2025-11-03 14:04:30 +0530

    price filter created

--- script.js
+++ script.js
@@ -81,6 +81,8 @@ async function fetchProducts() {
     try{
         let res = await fetch('products.json');
         let data = await res.json();
+        connectivityFiltering(data);
+        priceSliderFilter(data);
         data.forEach((product) => {
             productsContainer.innerHTML += `<div class="products">
             <div class="product-image">
@@ -111,27 +113,18 @@ fetchProducts();
 // filtering code
 
 
-let brandCheckBox = document.querySelectorAll('.brandCheck');
 
-let newPrductsNames = [];
+function brandFiltering() {
 
-brandCheckBox.forEach((checkBox) => {
-    checkBox.addEventListener('change', () => {
-        if (checkBox.checked) {
-            newPrductsNames.push(checkBox.value);
-        }else{
-            newPrductsNames = newPrductsNames.filter((name) => {
-                return name != checkBox.value;
-            })
-        }
-        
-        async function getBrandFiltered() {
+      async function getBrandFiltered() {
             let response = await fetch("products.json");
             let data = await response.json();
             
             let allNewArr = data.filter((product) => {
                 return newPrductsNames.includes(product.brandName)
             })
+            connectivityFiltering(allNewArr);
+            priceSliderFilter(allNewArr);
             productsContainer.innerHTML = '';
             allNewArr.forEach((newProduct) => {
                 productsContainer.innerHTML += `<div class="products">                                    <div class="product-image">
@@ -151,6 +144,130 @@ brandCheckBox.forEach((checkBox) => {
                                     </div>` 
             });
         }
+
+
+let brandCheckBox = document.querySelectorAll('.brandCheck');
+let newPrductsNames = [];
+brandCheckBox.forEach((checkBox) => {
+    checkBox.addEventListener('change', () => {
+        if (checkBox.checked) {
+            newPrductsNames.unshift(checkBox.value);
+        }else{
+            newPrductsNames = newPrductsNames.filter((name) => {
+                return name != checkBox.value;
+            })
+        }
         getBrandFiltered()
     });
 })
+
+}
+
+
+
+let connectCheck = document.querySelectorAll('.connectCheck');
+// let checkValues = [];
+
+function connectivityFiltering(allNewArr) {
+
+    connectCheck.forEach((connCheck) => {
+        connCheck.addEventListener('change', () => {
+            if (connCheck.checked) {
+                if (connCheck.value === "wired") {
+                    let wired = allNewArr.filter((product) => {
+                        return product.isWired === true;
+                    });
+                    productsContainer.innerHTML = '';
+                     wired.forEach((newProduct) => {
+                    productsContainer.innerHTML += `<div class="products">                                    <div class="product-image">
+                                    <img src=${newProduct.image} alt="">
+                                    <div class="fav-product-icon">
+                                    <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><path xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path></svg>
+                                    </div>
+                                    </div>
+                                    <div class="product-details">
+                                    <h3>${newProduct.name}</h3>
+                                    <div></div>
+                                    <div>
+                                    <span>₹${newProduct.realPrice}</span>
+                                     <span>₹${newProduct.price}</span>
+                                     </div>
+                                     </div>
+                                    </div>` 
+            });
+
+                }else{
+                    let wired = allNewArr.filter((product) => {
+                        return product.isWired === false;
+                    });
+                    productsContainer.innerHTML = '';
+                     wired.forEach((newProduct) => {
+                    productsContainer.innerHTML += `<div class="products">                                    <div class="product-image">
+                                    <img src=${newProduct.image} alt="">
+                                    <div class="fav-product-icon">
+                                    <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><path xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path></svg>
+                                    </div>
+                                    </div>
+                                    <div class="product-details">
+                                    <h3>${newProduct.name}</h3>
+                                    <div></div>
+                                    <div>
+                                    <span>₹${newProduct.realPrice}</span>
+                                     <span>₹${newProduct.price}</span>
+                                     </div>
+                                     </div>
+                                    </div>` 
+            });
+                }
+        } 
+        
+        });
+    });
+
+}
+
+
+
+brandFiltering();
+
+
+
+// price filter
+
+let minPrice = document.querySelector('.min-price');
+let maxPrice = document.querySelector('.max-price');
+let sliders = document.querySelectorAll('.slider');
+
+function priceSliderFilter(productArr) {
+    sliders.forEach((slider) => {
+    slider.addEventListener('change', () => {
+        let minPriceValue = minPrice.value;
+        let maxPriceValue = maxPrice.value;
+        let newPriceArr = productArr.filter((product) => {
+            if(product.price >= minPriceValue && product.price <= maxPriceValue){
+                return product
+            }
+        })
+        productsContainer.innerHTML = ''
+        newPriceArr.forEach((newProduct) => {
+             productsContainer.innerHTML += `<div class="products">                                    <div class="product-image">
+                                    <img src=${newProduct.image} alt="">
+                                    <div class="fav-product-icon">
+                                    <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><path xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path></svg>
+                                    </div>
+                                    </div>
+                                    <div class="product-details">
+                                    <h3>${newProduct.name}</h3>
+                                    <div></div>
+                                    <div>
+                                    <span>₹${newProduct.realPrice}</span>
+                                     <span>₹${newProduct.price}</span>
+                                     </div>
+                                     </div>
+                                    </div>` 
+        });
+        
+    });
+});    
+}
+
