7f6c63ef4ffb468efa98d5bc20c35845712d3703
Naji2402 <najisinan2402@gmail.com>
2025-11-03 09:57:56 +0530

    3/11/2025 brand filtering completed

--- script.js
+++ script.js
@@ -108,27 +108,33 @@ fetchProducts();
 
 
 
-
-
 // filtering code
 
 
 let brandCheckBox = document.querySelectorAll('.brandCheck');
-let allNewArr = [];
+
+let newPrductsNames = [];
 
 brandCheckBox.forEach((checkBox) => {
     checkBox.addEventListener('change', () => {
         if (checkBox.checked) {
-            async function getBrandFiltered() {
-                let response = await fetch("products.json");
-                let data = await response.json();
-                data.forEach((product) => {
-                    if (checkBox.value === product.brandName) {
-                        allNewArr.unshift(product)
-                        productsContainer.innerHTML = '';
-                        allNewArr.forEach((newProduct) => {
-                            productsContainer.innerHTML += `<div class="products">
-                                    <div class="product-image">
+            newPrductsNames.push(checkBox.value);
+        }else{
+            newPrductsNames = newPrductsNames.filter((name) => {
+                return name != checkBox.value;
+            })
+        }
+        
+        async function getBrandFiltered() {
+            let response = await fetch("products.json");
+            let data = await response.json();
+            
+            let allNewArr = data.filter((product) => {
+                return newPrductsNames.includes(product.brandName)
+            })
+            productsContainer.innerHTML = '';
+            allNewArr.forEach((newProduct) => {
+                productsContainer.innerHTML += `<div class="products">                                    <div class="product-image">
                                     <img src=${newProduct.image} alt="">
                                     <div class="fav-product-icon">
                                     <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><path xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path></svg>
@@ -139,16 +145,12 @@ brandCheckBox.forEach((checkBox) => {
                                     <div></div>
                                     <div>
                                     <span>₹${newProduct.realPrice}</span>
-                                    <span>₹${newProduct.price}</span>
-                                    </div>
-                                    </div>
-                                    </div>`
-                        });
-                    }
-                });
-            }
-            getBrandFiltered()
-    }
+                                     <span>₹${newProduct.price}</span>
+                                     </div>
+                                     </div>
+                                    </div>` 
+            });
+        }
+        getBrandFiltered()
     });
-    
-});
+})
